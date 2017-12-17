9152e29995cfc9f7483ba36aeae0096a2066d10b
gsingh11 <gurkeerat.singh@sofbang.com>
2017-12-17 17:28:38 +0530

    Application fixes for deployment on ACCS

---
+++ eu-chatbot-webhooks/index.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const express = require('express');
+var app = express();
+app.use(express.static('implementation'));
+
+var appConfig = require('./implementation/botConfig').get(process.env.NODE_ENV);
+var appjs = require('./implementation/app');
+
+//set paramters as appropriate
+var port = appConfig.port;
+const config = {
+    root: __dirname,
+    port: port,
+    logLevel: 'INFO',
+    logger: null,
+	basicAuth: null,
+    sslOptions: null
+};
+
+// Create an express app instance
+var express_app = appjs.init(config);
+
+// Start the server listening..
+express_app.listen(port);
