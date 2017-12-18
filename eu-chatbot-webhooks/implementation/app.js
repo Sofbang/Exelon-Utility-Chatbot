59c586665040b4efae40c6fc55cebb0266bcb2bb
gsingh11 <gurkeerat.singh@sofbang.com>
2017-12-18 12:05:31 +0530

    Added Alexa bot for BGE and COMED

--- eu-chatbot-webhooks/implementation/app.js
+++ eu-chatbot-webhooks/implementation/app.js
@@ -31,10 +31,15 @@ function init(config) {
     }
 
     //replace these settings to point to your webhook channel
-    var metadata = appConfig.alexa;
+    var comed_metadata = appConfig.channels.COMED.alexa;
+    var bge_metadata = appConfig.channels.BGE.alexa;
 
-    if (metadata.channelUrl && metadata.channelSecretKey) {
-        logger.info('Custom webhook is on');
+    if (bge_metadata.channelUrl && bge_metadata.channelSecretKey) {
+        logger.info("Alexa's Custom webhook for BGE is on");
+    }
+
+    if (comed_metadata.channelUrl && comed_metadata.channelSecretKey) {
+        logger.info("Alexa's Custom webhook for COMED is on");
     }
 
     function convertRespToSpeech(resp) {
@@ -56,7 +61,17 @@ function init(config) {
         return sentence;
     }
 
-    app.post('/singleBotWebhook/messages', bodyParser.json(), function(req, res) {
+    app.post('/singleBotWebhook/:opco/messages', bodyParser.json(), function (req, res) {
+        var opco = req.params.opco;
+        var metadata;
+        if (opco.toUpperCase() == 'BGE') {
+            metadata = bge_metadata;
+        } else if (opco.toUpperCase() == 'COMED') {
+            metadata = comed_metadata;
+        } else {
+            return res.status(400).send('Invalid Opco');
+        }
+
         //logger.info("Message from webhook channel", req.body);
         const userID = req.body.userId;
         if (!userID) {
@@ -71,100 +86,112 @@ function init(config) {
         }
     });
 
-    var alexa_app = new alexa.app("app");
-
-    alexa_app.intent("CommandBot", {},
-        function(alexa_req, alexa_res) {
-            var command = alexa_req.slot("command");
-            var session = alexa_req.getSession();
-            var userId = session.get("userId");
-            if (!userId) {
-                userId = randomIntInc(1000000, 9999999).toString();
-                session.set("userId", userId);
-            }
-            alexa_res.shouldEndSession(false);
-            if (metadata.channelUrl && metadata.channelSecretKey && userId && command) {
-                const userIdTopic = userId;
-                var respondedToAlexa = false;
-                var sendToAlexa = function() {
-                    if (!respondedToAlexa) {
-                        respondedToAlexa=true;
-                        alexa_res.send();
-                        logger.info('Prepare to send to Alexa');
-                        PubSub.unsubscribe(userIdTopic);
-                    } else {
-                        logger.info("Already sent response");
-                    }
-                }
-                var commandResponse = function(msg, data) {
-                    logger.info('Received callback message from webhook channel');
-                    var resp = data;
-                    logger.info('Parsed Message Body:', resp);
-                    if (!respondedToAlexa) {
-                        alexa_res.say(convertRespToSpeech(resp));
-                    } else {
-                        logger.info("Already processed response");
-                        return;
-                    }
-                    if (metadata.waitForMoreResponsesMs) {
-                        _.delay(function() {
-                            sendToAlexa();
-                        }, metadata.waitForMoreResponsesMs);
-                    }
-                };
-                var token = PubSub.subscribe(userIdTopic, commandResponse);
-                var additionalProperties = {
-                  "userProfile": {
-                    "clientType": "alexa"
-                  }
-                };
-                webhookUtil.messageToBotWithProperties(metadata.channelUrl, metadata.channelSecretKey, userId, command, additionalProperties, function(err) {
-                //webhookUtil.messageToBot(metadata.channelUrl, metadata.channelSecretKey, userId, command, function(err) {
-                    if (err) {
-                        logger.info("Failed sending message to Bot");
-                        alexa_res.say("Failed sending message to Bot.  Please review your bot configuration.");
-                        alexa_res.send();
-                        PubSub.unsubscribe(userIdTopic);
-                    }
-                });
-            } else {
-                _.defer(function() {
-                    alexa_res.say("I don't understand. Could you please repeat what you want?");
-                    alexa_res.send();
-                });
-            }
-            return false;
-        }
-    );
-
-    alexa_app.intent("AMAZON.StopIntent", {},
-        function(alexa_req, alexa_res) {
-            alexa_res.shouldEndSession(true);
-        }
-    );
-
-    alexa_app.launch(function(alexa_req, alexa_res) {
+    var handleCommandBot = function (alexa_req, alexa_res, metadata) {
+        var command = alexa_req.slot("command");
         var session = alexa_req.getSession();
-        session.set("startTime", Date.now());
-        alexa_res.say("Welcome to SingleBot. ");
-    });
+        var userId = session.get("userId");
+        if (!userId) {
+            userId = randomIntInc(1000000, 9999999).toString();
+            session.set("userId", userId);
+        }
+        alexa_res.shouldEndSession(false);
+        if (metadata.channelUrl && metadata.channelSecretKey && userId && command) {
+            const userIdTopic = userId;
+            var respondedToAlexa = false;
+            var sendToAlexa = function () {
+                if (!respondedToAlexa) {
+                    respondedToAlexa = true;
+                    alexa_res.send();
+                    logger.info('Prepare to send to Alexa');
+                    PubSub.unsubscribe(userIdTopic);
+                } else {
+                    logger.info("Already sent response");
+                }
+            }
+            var commandResponse = function (msg, data) {
+                logger.info('Received callback message from webhook channel');
+                var resp = data;
+                logger.info('Parsed Message Body:', resp);
+                if (!respondedToAlexa) {
+                    alexa_res.say(convertRespToSpeech(resp));
+                } else {
+                    logger.info("Already processed response");
+                    return;
+                }
+                if (metadata.waitForMoreResponsesMs) {
+                    _.delay(function () {
+                        sendToAlexa();
+                    }, metadata.waitForMoreResponsesMs);
+                }
+            };
+            var token = PubSub.subscribe(userIdTopic, commandResponse);
+            var additionalProperties = {
+                "userProfile": {
+                    "clientType": "alexa"
+                }
+            };
+            webhookUtil.messageToBotWithProperties(metadata.channelUrl, metadata.channelSecretKey, userId, command, additionalProperties, function (err) {
+                //webhookUtil.messageToBot(metadata.channelUrl, metadata.channelSecretKey, userId, command, function(err) {
+                if (err) {
+                    logger.info("Failed sending message to Bot");
+                    alexa_res.say("Failed sending message to Bot.  Please review your bot configuration.");
+                    alexa_res.send();
+                    PubSub.unsubscribe(userIdTopic);
+                }
+            });
+        } else {
+            _.defer(function () {
+                alexa_res.say("I don't understand. Could you please repeat what you want?");
+                alexa_res.send();
+            });
+        }
+        return false;
+    };
 
-    alexa_app.pre = function(alexa_req, alexa_res, alexa_type) {
-      logger.debug(alexa_req.data.session.application.applicationId);
-      // change the application id
-      if (alexa_req.data.session.application.applicationId != metadata.amzn_appId) {
-        logger.error("fail as application id is not valid");
-        alexa_res.fail("Invalid applicationId");
-      }
+    var handleStopIntent = function (alexa_req, alexa_res) {
+        alexa_res.shouldEndSession(true);
+    }
+
+    var handlePreEvent = function (alexa_req, alexa_res, alexa_type, metadata) {
+        logger.debug(alexa_req.data.session.application.applicationId);
+        // change the application id
+        if (alexa_req.data.session.application.applicationId != metadata.amzn_appId) {
+            logger.error("fail as application id is not valid");
+            alexa_res.fail("Invalid applicationId");
+        }
         logger.info(JSON.stringify(alexa_req.data, null, 4));
         if (!metadata.channelUrl || !metadata.channelSecretKey) {
             var message = "The singleBot cannot respond.  Please check the channel and secret key configuration.";
             alexa_res.fail(message);
             logger.info(message);
         }
-    };
-    alexa_app.express(app, "/", true);
+    }
 
+    // Alexa BGE Bot
+    var alexa_bge_app = new alexa.app("app", "bge");
+    alexa_bge_app.intent("CommandBot", {}, function (alexa_req, alexa_res) { handleCommandBot(alexa_req, alexa_res, bge_metadata); });
+    alexa_bge_app.intent("AMAZON.StopIntent", {}, handleStopIntent);
+    alexa_bge_app.launch(function(alexa_req, alexa_res) {
+        var session = alexa_req.getSession();
+        session.set("startTime", Date.now());
+        alexa_res.say("Welcome to BGE Bot. ");
+    });
+    alexa_bge_app.pre = function (alexa_req, alexa_res, alexa_type) { handlePreEvent(alexa_req, alexa_res, alexa_type, bge_metadata); };
+    alexa_bge_app.express(app, "/", true);
+
+    // Alexa COMED Bot
+    var alexa_comed_app = new alexa.app("app", "comed");
+    alexa_comed_app.intent("CommandBot", {}, function (alexa_req, alexa_res) { handleCommandBot(alexa_req, alexa_res, bge_metadata); });
+    alexa_comed_app.intent("AMAZON.StopIntent", {}, handleStopIntent);
+    alexa_comed_app.launch(function (alexa_req, alexa_res) {
+        var session = alexa_req.getSession();
+        session.set("startTime", Date.now());
+        alexa_res.say("Welcome to COMED Bot. ");
+    });
+    alexa_comed_app.pre = function (alexa_req, alexa_res, alexa_type) { handlePreEvent(alexa_req, alexa_res, alexa_type, bge_metadata); };
+    alexa_comed_app.express(app, "/", true);
+
+    // Following code handles bot for Google
     function handleEcho(req, res, botID) {
         sessionId = req.body.sessionId;
         console.log('google session ' + req.body.sessionId);
@@ -212,12 +239,12 @@ function init(config) {
     }
 	
     app.post('/comed/echo', function (req, res) {
-        var botID = appConfig.oracle.channels.COMED.id;
+        var botID = appConfig.channels.COMED.google.id;
         handleEcho(req, res, botID);
     });
 
     app.post('/bge/echo', function (req, res) {
-        var botID = appConfig.oracle.channels.BGE.id;
+        var botID = appConfig.channels.BGE.google.id;
         handleEcho(req, res, botID);
     });
 
@@ -225,12 +252,17 @@ function init(config) {
     app.locals.endpoints.push({
       name: 'webhook',
       method: 'POST',
-      endpoint: '/singleBotWebhook/messages'
+      endpoint: '/singleBotWebhook/:opco/messages'
     });
     app.locals.endpoints.push({
-      name: 'alexa',
-      method: 'POST',
-      endpoint: '/app'
+        name: 'alexa',
+        method: 'POST',
+        endpoint: '/app/comed'
+    });
+    app.locals.endpoints.push({
+        name: 'alexa',
+        method: 'POST',
+        endpoint: '/app/bge'
     });
     app.locals.endpoints.push({
       name: 'webhookGoogleComed',
--- eu-chatbot-webhooks/implementation/botConfig.js
+++ eu-chatbot-webhooks/implementation/botConfig.js
@@ -1,27 +1,33 @@
 ﻿/**
- * Created by Gurkirat Singh on 2017-02-21.
+ * Created by Gurkirat Singh on 11/12/2017.
  */
 var appConfig = {
     dev: {
         port: Number(process.env.PORT || 8080),
         logLevel: 'INFO',
-        alexa: {
-            waitForMoreResponsesMs: 200,
-            amzn_appId: "amzn1.ask.skill.08d961b6-ab5b-4859-a1ad-b02df9b0f82c",
-            channelSecretKey: 'iw2IkuMRId4f4YRfcL2obrk41pJ1fqQK',
-            channelUrl: 'http://bots-connectors:8000/connectors/v1/tenants/chatbot-tenant/listeners/webhook/channels/23349155-FCD6-4726-BE43-EB92F4FF140F'
-        },
-        google: {
-        },
-        oracle: {
-            channels: {
-                COMED: {
+        channels: {
+            COMED: {
+                google: {
                     id: '9B81EAA4-64C1-46B7-ACAC-9C047EAB7C35',
                     name: 'ExelonCOMEDUtilityDev'
                 },
-                BGE: {
+                alexa: {
+                    waitForMoreResponsesMs: 200,
+                    amzn_appId: "amzn1.ask.skill.08d961b6-ab5b-4859-a1ad-b02df9b0f82c",
+                    channelSecretKey: 'iw2IkuMRId4f4YRfcL2obrk41pJ1fqQK',
+                    channelUrl: 'http://bots-connectors:8000/connectors/v1/tenants/chatbot-tenant/listeners/webhook/channels/23349155-FCD6-4726-BE43-EB92F4FF140F'
+                }
+            },
+            BGE: {
+                google: {
                     id: '0790802F-43A9-47A4-9AE9-34C7611B78C6',
                     name: 'ExelonBGEUtilityDev'
+                },
+                alexa: {
+                    waitForMoreResponsesMs: 200,
+                    amzn_appId: "amzn1.ask.skill.08d961b6-ab5b-4859-a1ad-b02df9b0f82c",
+                    channelSecretKey: 'iw2IkuMRId4f4YRfcL2obrk41pJ1fqQK',
+                    channelUrl: 'http://bots-connectors:8000/connectors/v1/tenants/chatbot-tenant/listeners/webhook/channels/23349155-FCD6-4726-BE43-EB92F4FF140F'
                 }
             }
         }
@@ -29,23 +35,29 @@ var appConfig = {
     stage: {
         port: 9100,
         logLevel: 'INFO',
-        alexa: {
-            waitForMoreResponsesMs: 200,
-            amzn_appId: "amzn1.ask.skill.08d961b6-ab5b-4859-a1ad-b02df9b0f82c",
-            channelSecretKey: 'iw2IkuMRId4f4YRfcL2obrk41pJ1fqQK',
-            channelUrl: 'http://bots-connectors:8000/connectors/v1/tenants/chatbot-tenant/listeners/webhook/channels/23349155-FCD6-4726-BE43-EB92F4FF140F'
-        },
-        google: {
-        },
-        oracle: {
-            channels: {
-                COMED: {
+        channels: {
+            COMED: {
+                google: {
                     id: '6B7DB2BA-CA8F-40FD-9B56-820F6A348EFB',
                     name: 'ExelonCOMEDUtilityTest'
                 },
-                BGE: {
+                alexa: {
+                    waitForMoreResponsesMs: 200,
+                    amzn_appId: "amzn1.ask.skill.08d961b6-ab5b-4859-a1ad-b02df9b0f82c",
+                    channelSecretKey: 'iw2IkuMRId4f4YRfcL2obrk41pJ1fqQK',
+                    channelUrl: 'http://bots-connectors:8000/connectors/v1/tenants/chatbot-tenant/listeners/webhook/channels/23349155-FCD6-4726-BE43-EB92F4FF140F'
+                }
+            },
+            BGE: {
+                google: {
                     id: '77CCE6DA-2E05-4DED-87D9-25EE1167E29C',
                     name: 'ExelonBGEUtilityTest'
+                },
+                alexa: {
+                    waitForMoreResponsesMs: 200,
+                    amzn_appId: "amzn1.ask.skill.08d961b6-ab5b-4859-a1ad-b02df9b0f82c",
+                    channelSecretKey: 'iw2IkuMRId4f4YRfcL2obrk41pJ1fqQK',
+                    channelUrl: 'http://bots-connectors:8000/connectors/v1/tenants/chatbot-tenant/listeners/webhook/channels/23349155-FCD6-4726-BE43-EB92F4FF140F'
                 }
             }
         }
@@ -53,21 +65,29 @@ var appConfig = {
     prod: {
         port: 9000,
         logLevel: 'INFO',
-        alexa: {
-            waitForMoreResponsesMs: 200,
-            amzn_appId: "amzn1.ask.skill.08d961b6-ab5b-4859-a1ad-b02df9b0f82c",
-            channelSecretKey: 'iw2IkuMRId4f4YRfcL2obrk41pJ1fqQK',
-            channelUrl: 'http://bots-connectors:8000/connectors/v1/tenants/chatbot-tenant/listeners/webhook/channels/23349155-FCD6-4726-BE43-EB92F4FF140F'
-        },
-        google: {
-        },
-        oracle: {
-            channels: {
-                COMED: {
-                    id: '9B81EAA4-64C1-46B7-ACAC-9C047EAB7C35'
+        channels: {
+            COMED: {
+                google: {
+                    id: '6B7DB2BA-CA8F-40FD-9B56-820F6A348EFB',
+                    name: 'ExelonCOMEDUtilityTest'
                 },
-                BGE: {
-                    id: '0790802F-43A9-47A4-9AE9-34C7611B78C6'
+                alexa: {
+                    waitForMoreResponsesMs: 200,
+                    amzn_appId: "amzn1.ask.skill.08d961b6-ab5b-4859-a1ad-b02df9b0f82c",
+                    channelSecretKey: 'iw2IkuMRId4f4YRfcL2obrk41pJ1fqQK',
+                    channelUrl: 'http://bots-connectors:8000/connectors/v1/tenants/chatbot-tenant/listeners/webhook/channels/23349155-FCD6-4726-BE43-EB92F4FF140F'
+                }
+            },
+            BGE: {
+                google: {
+                    id: '77CCE6DA-2E05-4DED-87D9-25EE1167E29C',
+                    name: 'ExelonBGEUtilityTest'
+                },
+                alexa: {
+                    waitForMoreResponsesMs: 200,
+                    amzn_appId: "amzn1.ask.skill.08d961b6-ab5b-4859-a1ad-b02df9b0f82c",
+                    channelSecretKey: 'iw2IkuMRId4f4YRfcL2obrk41pJ1fqQK',
+                    channelUrl: 'http://bots-connectors:8000/connectors/v1/tenants/chatbot-tenant/listeners/webhook/channels/23349155-FCD6-4726-BE43-EB92F4FF140F'
                 }
             }
         }
