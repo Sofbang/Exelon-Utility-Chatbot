1d9d23f21482950f613496edeb43624b45157cee
gsingh11 <gurkeerat.singh@sofbang.com>
2017-12-22 17:13:34 +0530

    settings updated for socket app

--- eu-websocket-app/lib/admin/config.js
+++ eu-websocket-app/lib/admin/config.js
@@ -2,6 +2,6 @@
  * Created by YPANSHIN on 2017-02-21.
  */
 var chatBotConfiguration = {
-  uri: 'https://EUWebsocketApp-sofbangmobile.uscom-central-1.oraclecloud.com:443',
+    uri: 'https://sfbdemo4.ngrok.io',
   channelsEndPoint: '/chat/botChannels'
 };
