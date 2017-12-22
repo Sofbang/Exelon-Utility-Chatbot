0366e06f63e814a44ed3afdf74d4cff233439924
gsingh11 <gurkeerat.singh@sofbang.com>
2017-12-22 16:49:31 +0530

    updated Settings

--- eu-chatbot-webhooks/implementation/botConfig.js
+++ eu-chatbot-webhooks/implementation/botConfig.js
@@ -4,30 +4,30 @@
 var appConfig = {
     port: Number(process.env.PORT || 8080),
     logLevel: 'INFO',
-    socketHost: process.env.SOCKET_HOST || 'euwebsocketapp-sofbangmobile.uscom-central-1.oraclecloud.com',
+    socketHost: process.env.SOCKET_HOST || 'sfbdemo4.ngrok.io',
     channels: {
         COMED: {
             google: {
-                id: process.env.GOOGLE_COMED || '8501F9A5-E719-4716-BD0C-A397F1A0EAAE',
-                name: 'ExelonCOMEDUtilityTest'
-            },
-            alexa: {
-                waitForMoreResponsesMs: Number(process.env.WAIT_TIME || 200),
-                amzn_appId: process.env.ALEXA_COMED_APP_ID || "amzn1.ask.skill.78de290d-b2ef-4003-89a5-ebaecbfacbf6",
-                channelSecretKey: process.env.COMED_CHANNEL_SECRET || 'WmPsDhl1zhSYZ69r1Tvt9woDjafAaXZu',
-                channelUrl: process.env.COMED_CHANNEL_URL || 'https://devomceBOTSCON-sofbangomce.uscom-central-1.oraclecloud.com:443/connectors/v1/tenants/idcs-b36382730d49427eb3b31e7f99a5aa84/listeners/webhook/channels/C4925EDD-1CCD-4904-B745-936681AF24E0'
-            }
-        },
-        BGE: {
-            google: {
-                id: process.env.GOOGLE_BGE || '4F0A34BB-9620-492B-8827-2A1B37C5E2CB',
+                id: process.env.GOOGLE_BGE || 'E83BDF42-702D-42EA-9D56-7E979D184695',
                 name: 'ExelonBGEUtilityTest'
             },
             alexa: {
                 waitForMoreResponsesMs: process.env.WAIT_TIME || 200,
-                amzn_appId: process.env.ALEXA_BGE_APP_ID || "amzn1.ask.skill.1a558578-efd6-4c3a-b094-40f5868b7334",
-                channelSecretKey: process.env.BGE_CHANNEL_SECRET || 'eGVPnxk9JbXLeol1cIS5aOhCC3UPoNVN',
-                channelUrl: process.env.BGE_CHANNEL_URL || 'https://devomceBOTSCON-sofbangomce.uscom-central-1.oraclecloud.com:443/connectors/v1/tenants/idcs-b36382730d49427eb3b31e7f99a5aa84/listeners/webhook/channels/4098EEF6-E0CE-4103-B9DC-D21753C3DCFC'
+                amzn_appId: process.env.ALEXA_BGE_APP_ID || "amzn1.ask.skill.ae00ef8d-e0ff-4c13-af86-07c520c289c4",
+                channelSecretKey: process.env.BGE_CHANNEL_SECRET || 'V1sCIK2cc7qDRocj3rUnPEtkLmuWMR7D',
+                channelUrl: process.env.BGE_CHANNEL_URL || 'https://sfbdemo2.ngrok.io/connectors/v1/tenants/chatbot-tenant/listeners/webhook/channels/1B8BD904-E775-4637-8DAA-E9CB27DC7CE7'
+            }
+        },
+        BGE: {
+            google: {
+                id: process.env.GOOGLE_BGE || '85E7C890-B6D4-4CE8-9452-C468F89E1E6D',
+                name: 'ExelonBGEUtilityTest'
+            },
+            alexa: {
+                waitForMoreResponsesMs: process.env.WAIT_TIME || 200,
+                amzn_appId: process.env.ALEXA_BGE_APP_ID || "amzn1.ask.skill.698a7a2b-804f-4da4-b85a-7c1c8a70fdee",
+                channelSecretKey: process.env.BGE_CHANNEL_SECRET || 'cabgaAZ5oi15m6oT8zukMMMvwNoPMWvP',
+                channelUrl: process.env.BGE_CHANNEL_URL || 'https://sfbdemo2.ngrok.io/connectors/v1/tenants/chatbot-tenant/listeners/webhook/channels/E7C634DF-725B-4A3C-91A0-E90F44582D7D'
             }
         }
     }
