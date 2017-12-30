830661ea00659264887bd52966eb0ecd373627b2
Puneet Gandhi <puneet.gandhi@sofbang.com>
2017-12-30 20:12:20 +0530

    OMCE APIS

---
+++ MCS-Implementation/ExelonOMCEAPIs/euchatbotbgecc/botservices/CheckBalance.js
@@ -0,0 +1,65 @@
+"use strict";
+
+var log4js = require('log4js');
+var logger = log4js.getLogger();
+var ExelonService = require('./ExelonService');
+var moment = require('moment');
+
+module.exports = {
+
+	metadata: function metadata() {
+        return {
+            "name": "CheckBalance",
+            "properties": {
+                "PhoneNumber": { "type": "string", "required": true },
+                "Identifier": { "type": "string", "required": true },
+                "AccountNumber": { "type": "string", "required": false }
+            },
+            "supportedActions": []
+        };
+    },
+
+    invoke: function invoke(conversation, done) {
+        var PhoneNumber = conversation.properties().PhoneNumber;
+        var AccountNumber = conversation.properties().AccountNumber;
+        var Identifier = conversation.properties().Identifier;
+        var mobileSdk = conversation.oracleMobile;
+        console.log("Account Number is : "+AccountNumber+" Phone Number is : "+PhoneNumber+" and Identifier is : "+Identifier);
+        ExelonService.checkBalance(mobileSdk, AccountNumber, PhoneNumber, Identifier).then(function (response) {
+            console.log("response :"+JSON.stringify(response));
+            if(response.success){
+                    if(response.data.address){
+                        conversation.variable("servicesDown","false");
+                        conversation.variable("addressFound","true");
+                        conversation.variable("multipleAddressFound","false");
+                        conversation.variable("checkBalance_MaskedAddress", "My records indicate that the address associated with this account begins with "+ response.data.address);
+                        conversation.variable("checkBalance_AccountInfo", "You have $"+response.data.remainingBalanceDue+" due on "+moment(response.data.dueByDate).format("MMMM DD, YYYY")+". For a full breakdown of your bill, please login to My Account at bge.com or call us at 1-800-685-0123.");
+                    }else{
+                        conversation.variable("servicesDown","false");
+                        conversation.variable("addressFound","false");
+                    }
+                    conversation.transition();
+                    done(); 
+            }else{
+                if(response.meta.code == "FN-MULTIPLE-ACCOUNTS"){
+                            conversation.variable("servicesDown","false");
+                            conversation.variable("addressFound","true");
+                            conversation.variable("multipleAddressFound","true");
+                            conversation.transition();
+                            done(); 
+                } else {
+                    conversation.variable("servicesDown","false");
+                    conversation.variable("addressFound","false");
+                    conversation.transition();
+                    done();                     
+                }
+            }
+        }).catch(function(err){
+                    conversation.variable("servicesDown","true");
+                    conversation.transition();
+                    done(); 
+                    console.log("error not handled at checkBalance service :"+ err);
+        });
+    }
+
+}
---
+++ MCS-Implementation/ExelonOMCEAPIs/euchatbotbgecc/botservices/utils.js
@@ -0,0 +1,32 @@
+﻿var MESSAGES = require('./Messages.json');
+var moment = require('moment-timezone');
+var USTimeZone = "America/Toronto";
+
+module.exports = {
+
+    getMessageForBot: function (msg, estRestore, qyCustAffected) {
+        for (var k in MESSAGES) {
+            var MessageTemplate = MESSAGES[k].MessageToBeReplaced;
+            var parameters = MESSAGES[k].parameters;
+            var MessageToBeReplaced = MessageTemplate;
+            var ReplacedMessage = MESSAGES[k].ReplacedMessage;
+            if (parameters) {
+                var now = moment().tz(USTimeZone).format("hh:mm A") + ' on ' + moment().tz(USTimeZone).format("MM/DD/YYYY");
+                MessageToBeReplaced = MessageToBeReplaced.replace("{estRestore}", moment(estRestore).tz(USTimeZone).format("MM/DD/YYYY hh:mm A"));
+                MessageToBeReplaced = MessageToBeReplaced.replace("{qyCustAffected}", qyCustAffected);
+                MessageToBeReplaced = MessageToBeReplaced.replace("{now}", now);
+
+                ReplacedMessage = ReplacedMessage.replace("{estRestore}", moment(estRestore).tz(USTimeZone).format("MM/DD/YYYY") + ' at ' + moment(estRestore).tz(USTimeZone).format("hh:mm A"));
+                ReplacedMessage = ReplacedMessage.replace("{qyCustAffected}", qyCustAffected);
+                ReplacedMessage = ReplacedMessage.replace("{now}", now);
+            }
+            var replacementIndex = msg.indexOf(MessageToBeReplaced);
+            if (replacementIndex != -1) {
+                msg = ReplacedMessage;
+                break;
+            }
+        }
+        return msg;
+    }
+
+}
