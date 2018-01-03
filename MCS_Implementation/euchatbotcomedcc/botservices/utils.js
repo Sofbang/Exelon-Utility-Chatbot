957c7f189139fe9f38056fc6e0d0612ab149734c
gsingh11 <gurkeerat.singh@sofbang.com>
2018-01-03 14:35:48 +0530

    Worked on Customer Affected message (#EXECHATBOT-53)

--- MCS_Implementation/euchatbotcomedcc/botservices/utils.js
+++ MCS_Implementation/euchatbotcomedcc/botservices/utils.js
@@ -4,24 +4,29 @@ var USTimeZone = "America/Chicago";
 
 module.exports = {
 
-    getMessageForBot: function (msg, estRestore, qyCustAffected) {
+    getMessageForBot: function (msg, estRestore) {
+        var CUSTOMER_MESSAGE = "customer(s) are affected by a power outage in your area.";
+        var custAffectedIndex = msg.indexOf(CUSTOMER_MESSAGE);
+        if (custAffectedIndex != -1) {
+            msg = msg.replace("we indicate that", "my records indicate that");
+        }
         for (var k in MESSAGES) {
-            var MessageTemplate = MESSAGES[k].MessageToBeReplaced;
-            var parameters = MESSAGES[k].parameters;
-            var MessageToBeReplaced = MessageTemplate;
+            var MessageToBeReplaced = MESSAGES[k].MessageToBeReplaced;
             var ReplacedMessage = MESSAGES[k].ReplacedMessage;
-            if (parameters) {
-                var now = moment().tz(USTimeZone).format("h:mm A") + ' on ' + moment().tz(USTimeZone).format("M/D/YYYY");
-                MessageToBeReplaced = MessageToBeReplaced.replace("{estRestore}", moment(estRestore).tz(USTimeZone).format("MM/DD/YYYY hh:mm A"));
-                MessageToBeReplaced = MessageToBeReplaced.replace("{qyCustAffected}", qyCustAffected);
-                MessageToBeReplaced = MessageToBeReplaced.replace("{now}", now);
+            var parameters = MESSAGES[k].parameters;
+            // Following code is currently not required in case of COMED
+            //if (parameters) {
+            //    var now = moment().tz(USTimeZone).format("h:mm A") + ' on ' + moment().tz(USTimeZone).format("M/D/YYYY");
+            //    MessageToBeReplaced = MessageToBeReplaced.replace("{estRestore}", moment(estRestore).tz(USTimeZone).format("MM/DD/YYYY hh:mm A"));
+            //    MessageToBeReplaced = MessageToBeReplaced.replace("{qyCustAffected}", qyCustAffected);
+            //    MessageToBeReplaced = MessageToBeReplaced.replace("{now}", now);
 
-                ReplacedMessage = ReplacedMessage.replace("{estRestore}", moment(estRestore).tz(USTimeZone).format("MM/DD/YYYY") + ' at ' + moment(estRestore).tz(USTimeZone).format("hh:mm A"));
-                ReplacedMessage = ReplacedMessage.replace("{qyCustAffected}", qyCustAffected);
-                ReplacedMessage = ReplacedMessage.replace("{now}", now);
-            }
-            console.log("MessageToBeReplaced: " + JSON.stringify(MessageToBeReplaced));
-            console.log("msg before replacement: " + JSON.stringify(msg));
+            //    ReplacedMessage = ReplacedMessage.replace("{estRestore}", moment(estRestore).tz(USTimeZone).format("MM/DD/YYYY") + ' at ' + moment(estRestore).tz(USTimeZone).format("hh:mm A"));
+            //    ReplacedMessage = ReplacedMessage.replace("{qyCustAffected}", qyCustAffected);
+            //    ReplacedMessage = ReplacedMessage.replace("{now}", now);
+            //}
+            //console.log("MessageToBeReplaced: " + JSON.stringify(MessageToBeReplaced));
+            //console.log("msg before replacement: " + JSON.stringify(msg));
             msg = msg.replace(MessageToBeReplaced, ReplacedMessage);
             if (msg == ReplacedMessage) {
                 break;
